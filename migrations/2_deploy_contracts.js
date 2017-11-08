1e31978de90337e1b2549b9ef0e22bf569ac140e
prc <prcolaco@gmail.com>
2017-11-08 20:30:06 +0000

    Removed all other contracts besides the MN8 token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,13 @@
 var MN8 = artifacts.require("./MN8.sol");
-var Auth = artifacts.require("./Auth.sol");
-var Stem = artifacts.require("./Stem.sol");
-var Artist = artifacts.require("./Artist.sol");
+// var Auth = artifacts.require("./Auth.sol");
+// var Stem = artifacts.require("./Stem.sol");
+// var Artist = artifacts.require("./Artist.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(MN8);
-  deployer.deploy(Auth);
-  deployer.deploy(Stem);
-  deployer.link(Auth, Artist);
-  deployer.link(Stem, Artist);
-  deployer.deploy(Artist);
+  // deployer.deploy(Auth);
+  // deployer.deploy(Stem);
+  // deployer.link(Auth, Artist);
+  // deployer.link(Stem, Artist);
+  // deployer.deploy(Artist);
 };
