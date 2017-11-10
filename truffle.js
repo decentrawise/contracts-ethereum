f9bf436a4ed0105cfcf077a7065b24e3b0ce183b
prc <prcolaco@gmail.com>
2017-11-10 07:35:51 +0000

    Added rinkeby test network configuration

--- truffle.js
+++ truffle.js
@@ -5,6 +5,13 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
       gas: 4600000
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      from: "0x161E92E2801BFC62E9F890ff2528F4aB11D7756E",
+      network_id: 4,
+      gas: 4612388
     }
   }
 };
